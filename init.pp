2bc238ec03ee019fb219344661f4310bd5ec0b79
jaivikshrestha <vikshrestha5050@gmail.com>
2017-02-02 12:00:09 -0600

    final commit

--- init.pp
+++ init.pp
@@ -1,8 +1,21 @@
 class html {
-  file { '/var/www/html/index.html' :
+  file { 'html.pp' :
+    name   => '/var/www/html/index.html',
     ensure => 'file',
     source => 'puppet:///modules/html/html.pp',
   }
+
+  package { 'httpd' :
+    ensure => 'present',
+  }
+
+  service { 'httpd' :
+    ensure  => 'running', 
+    enable  => true,
+    require => Package['httpd'],
+    subscribe => File['html.pp'],
+  }
+
      
 }
 
